85a55973fe46da9856ad329f672e56117369ea87
Muskaan Shaikh <muskaan.m.y.shaikh@gmail.com>
2025-11-09 15:46:49 -0500

    Favicon update

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,36 +4,43 @@ import React from 'react'
 import { MdOutlineCreate, MdOutlineLightMode, MdOutlineDarkMode } from 'react-icons/md';
 import { PiSidebarFill } from 'react-icons/pi';
 import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import ProfileDropdown from './ProfileDropdown';
 
 export default function Header({sidebar, toggleSidebar, handleNewChat, isDarkMode, toggleTheme}) {
+  const pathname = usePathname();
+  const isAdminRoute = typeof pathname === 'string' && pathname.startsWith('/admin');
+
   return (
     <header className="p-6 flex items-center justify-between">
-      <div className="flex space-x-6 items-center">
+      <Link href="/" aria-label="Go to home" className="z-10 flex space-x-6 items-center cursor-pointer">
         <Image 
           src="https://luminascreens.com/wp-content/uploads/2023/05/canva-photo-editor-52-2.webp"
           alt="Lumina Logo"
           width={200}
           height={200}
           priority
-          className="object-contain"
         />
-      </div>
+      </Link>
 
       <div className="flex items-center space-x-4">
-        <div className="w-10 h-10 border border-gray-300 dark:border-gray-500 rounded-full flex items-center justify-center">
-          {!isDarkMode ? (
-            <MdOutlineLightMode
-              className="w-6 h-6 cursor-pointer text-secondary-dark"
-              onClick={toggleTheme}
-            />
-          ) : (
-            <MdOutlineDarkMode
-              className="w-6 h-6 cursor-pointer text-secondary-light"
-              onClick={toggleTheme}
-            />
-          )}
-        </div>
+        {/* Hide theme toggle in admin console */}
+        {!isAdminRoute && (
+          <div className="w-10 h-10 border border-gray-300 dark:border-gray-500 rounded-full flex items-center justify-center">
+            {!isDarkMode ? (
+              <MdOutlineLightMode
+                className="w-6 h-6 cursor-pointer text-secondary-dark"
+                onClick={toggleTheme}
+              />
+            ) : (
+              <MdOutlineDarkMode
+                className="w-6 h-6 cursor-pointer text-secondary-light"
+                onClick={toggleTheme}
+              />
+            )}
+          </div>
+        )}
         <ProfileDropdown />
       </div>
     </header>
